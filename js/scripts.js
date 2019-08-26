ca9825202cd555fd722d57785ca01eb5928583cf
Kevin Hinojosa <kevi123321@gmail.com>
2019-08-25 23:11:29 -0700

    added music via jaascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 var clickSound = new Audio("audio/summoningSound.mp3");
+var clickSound2 = new Audio("https://ia802506.us.archive.org/28/items/ThePeoplesCourtThemeSong/The%20People%27s%20Court%20Theme%20Song%21.mp3");
+
 
 
 document.getElementById('v-pills-home-tab').addEventListener("click", clickFunction);
-
 document.getElementById('v-pills-messages-tab').addEventListener("click", clickFunction);
-
-
 document.getElementById('v-pills-settings-tab').addEventListener("click", clickFunction);
 
 
+
+clickSound2.play();
 // This function plays an audio sound
 function clickFunction(e){
   clickSound.play();
