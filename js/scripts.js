68dda5b8ab90db1836d32c66c19b4393021105ca
Kevin Hinojosa <kevi123321@gmail.com>
2019-08-26 01:05:44 -0700

    logo created, edited points

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 var clickSound = new Audio("audio/summoningSound.mp3");
-var clickSound2 = new Audio("https://ia802506.us.archive.org/28/items/ThePeoplesCourtThemeSong/The%20People%27s%20Court%20Theme%20Song%21.mp3");
+var peoplesCourtTheme = new Audio("https://ia802506.us.archive.org/28/items/ThePeoplesCourtThemeSong/The%20People%27s%20Court%20Theme%20Song%21.mp3");
 
+peoplesCourtTheme.play();
 
 
 document.getElementById('v-pills-home-tab').addEventListener("click", clickFunction);
 document.getElementById('v-pills-messages-tab').addEventListener("click", clickFunction);
 document.getElementById('v-pills-settings-tab').addEventListener("click", clickFunction);
 
-
-
-clickSound2.play();
 // This function plays an audio sound
 function clickFunction(e){
   clickSound.play();
   // alert("audio played");
   }
+
+document.getElementById('logo').addEventListener("click", clickFunction2);
+
+function clickFunction2(e){
+  peoplesCourtTheme.play();
+  alert("The Peoples' Court is In Session");
+}
